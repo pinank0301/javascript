36af18f63fb3eb59580e979073b86493931fb2f7
PINANK JATIN TRIVEDI <125110536+pinank0301@users.noreply.github.com>
2025-12-03 14:37:10 +0000

    datatypes done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict" // treat all JS code as newer version
+// alert(3 + 3) // we are using node JS, not browser
+console.log(3 
+    + 
+    3) // not a good practice, code readability should be high
+console.log("Pinank")
+
+let name = "Pinank" //string
+let age = 20 //num
+let isLoggedin = true //boolean
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => "", ''
+// boolean => true, false
+// null => standalone value
+// undefined =>
+// symbol => to find uniqueness
+
+
+// objects
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
+
